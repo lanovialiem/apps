82beb6079e350f705dfcf19eaf32118b83938cf3
lanovialiem <lonovia.liem21@gmail.com>
2026-05-21 14:21:45 +0700

    approval_history

--- app/Models/ApprovalHistory.php
+++ app/Models/ApprovalHistory.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class ApprovalHistory extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'penawaran_id',
+        'user_id',
+        'name',
+        'role',
+        'notes',
+        'status',
+        'level'
+    ];
 }
